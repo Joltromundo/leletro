e6b7c74c13f016d5282fc39a820c77ff808e54bf
adalbeto dornelas <jonathan.tibia@hotmail.com>
2023-07-31 12:48:35 -0300

    Finishing front-end adjustments

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -2,8 +2,8 @@
 
 <template>
   <div>
-    <div class="relative isolate px-6 pt-14 lg:px-8">
-      <div class="mx-auto max-w-2xl py-20 sm:py-38 lg:py-46">
+    <div class="relative isolate px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl py-10">
         <div class="hidden sm:mb-8 sm:flex sm:justify-center">
           <div
             class="relative rounded-full px-3 py-1 text-sm leading-6 text-slate-300 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -15,10 +15,10 @@
           <h1 class="text-4xl font-bold tracking-tight text-slate-100 sm:text-6xl">Create news Appliances registers</h1>
           <p class="mt-6 text-lg leading-8 text-slate-300">Keep in control your data with this panel, that helps to see all your Appliances! Let's start?</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#"
-              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">See Appliances</a>
-            <a href="#" class="text-sm font-semibold leading-6 text-slate-100">Create new <span
-                aria-hidden="true">→</span></a>
+            <router-link to="/appliance"
+              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">See Appliances</router-link>
+            <router-link to="/appliance/create" class="text-sm font-semibold leading-6 text-slate-100">Create new <span
+                aria-hidden="true">→</span></router-link>
           </div>
         </div>
       </div>
---
+++ src/components/icons/IconLogoSimple.vue
@@ -0,0 +1,15 @@
+<template>
+    <svg id="Q8F4JJUdqp" viewBox="0 0 369.89473684210526 87.70170222239875" class="w-30 h-8">
+        <defs id="SvgjsDefs1078"></defs>
+        <g id="SvgjsG1079" featurekey="symbolFeature-0"
+            transform="matrix(2.1925425555599687,0,0,2.1925425555599687,-24.117968111159655,-26.310510666719622)"
+            fill="#fff">
+            <title xmlns="http://www.w3.org/2000/svg">home
+                appliances-electronic-electric-household-electricity</title>
+            <polygon xmlns="http://www.w3.org/2000/svg"
+                points="51 42 51 21 36 12 21 21 21 42 30 42 30 27 42 27 42 42 51 42"></polygon>
+            <path xmlns="http://www.w3.org/2000/svg"
+                d="M23,50H47.18a3,3,0,1,0,0-2H24V47a3,3,0,0,0-6,0v2a1,1,0,0,1-2,0V30h.5c.83,0,1.5-.5,1.5-1.11V26.67a1,1,0,0,0-.28-.65A3.43,3.43,0,0,0,19,23.4V21H18V19a1,1,0,0,0-2,0v2H14V19a1,1,0,0,0-2,0v2H11v2.4A3.43,3.43,0,0,0,12.28,26a1,1,0,0,0-.28.65v2.22c0,.61.67,1.11,1.5,1.11H14V49a3,3,0,0,0,6,0V47a1,1,0,0,1,2,0v2A1,1,0,0,0,23,50Z"></path>
+        </g>
+    </svg>
+</template>
